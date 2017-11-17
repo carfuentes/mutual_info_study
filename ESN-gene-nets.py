ce5e632395e8f28097b75b19eb950c0dc60eee5c
carfuentes <carfuentesrosales@gmail.com>
2017-11-17 15:25:56 +0100

    NB from last commit converted

--- ESN-gene-nets.py
+++ ESN-gene-nets.py
@@ -75,84 +75,92 @@ def testing_gene_net_file(directory,file):
 # In[7]:
 
 
-def testing_gene_net_derivative(directory,a,b,c,n):
+def testing_gene_net_derivative(directory,a,b,c,n,i_max=80):
+    #init
     csv_files= [file for file in os.listdir(directory) if file.startswith("network_edge_list")]
     Y_by_file={}
     X_by_file={}
     MI_by_file={}
     NRMSE_by_file={}
+    
     for file in csv_files:
+        #init
         print(file)
         filename=file[file.index("list")+5:file.index(".csv")]
+        
+        #Run network
         net=ESN(os.path.join(directory, file),1,1,0.95)
         net.initialize()
         net.collect_states_derivative(a,b,c,initLen,trainLen,testLen)
         net.calculate_weights_derivative(initLen,trainLen,n)
         net.run_predictive_derivative(a,b,c,testLen,trainLen)
+        
+        #Calculate output
         X_by_file[filename]=net.u
         Y_by_file[filename]=net.Y
-        NRMSE_by_file[filename]=nrmse_n(net.Y,net.u)
-        print(nrmse(net.Y[0,0:errorLen],net.u[trainLen+1:trainLen+errorLen+1]))
         MI_by_file[filename]=memory_capacity_n(net.Y, net.u,n)
+        NRMSE_by_file[filename]=nrmse_n(net.Y,net.u,i_max,errorLen,trainLen)
+        
+        #prints
+        
+        print(nrmse(net.Y[0,0:errorLen],net.u[trainLen+1:trainLen+errorLen+1]))
         print(net.res_size, " FINISHED")
-        
-        #figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
-        #plot( net.u[trainLen+1:trainLen+testLen+1], 'g' )
-        #plot( net.Y.T, 'b' )
-        #xlabel("time")
-        #ylabel("signal")
-        #title('Target and generated signals $y(n)$ starting at $n=0$ until $n=%s$' %testLen)
-        #legend(['Target signal', 'Free-running predicted signal'])
-        #savefig("plots-input-vs-output/%s_testLen" %filename)
-        #show()
-        
-        #figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
-        #plot( net.u[trainLen+1:trainLen+50+1], 'g' )
-        #plot( net.Y.T[0:50], 'b' )
-        #xlabel("time")
-        #ylabel("signal")
-        #title('Target and generated signals $y(n)$ starting at $n=0$ until $n=50$')
-        #legend(['Target signal', 'Free-running predicted signal'])
-        #savefig("plots-input-vs-output/%s_50" %filename)
-        #show()
-       
-  
-    return MI_by_file, X_by_file, Y_by_file, NRMSE_by_file
+    return X_by_file, Y_by_file, NRMSE_by_file, MI_by_file
   
 
 
 # In[8]:
 
 
-def testing_gene_net_derivative_file(directory,file,a,b,c,n):
+def testing_gene_net_derivative_file(directory,file,a,b,c,n,i_max=80):
+    #init
     print(file)
     filename=file[file.index("list")+5:file.index(".csv")]
+    
+    #Run network
     net=ESN(os.path.join(directory, file),1,1,0.95)
     net.initialize()
     net.collect_states_derivative(a,b,c,initLen,trainLen,testLen)
     net.calculate_weights_derivative(initLen,trainLen,n)
     net.run_predictive_derivative(a,b,c,testLen,trainLen)
+    
+    #Calculate output
     X=net.u
     Y=net.Y
-    nrmse_i=nrmse_n(net.Y,net.u,errorLen,trainLen)
-    print(sqrt(mean_squared_error(net.u[trainLen+1:trainLen+errorLen+1],net.Y[0,0:errorLen]))/np.std(net.u[trainLen+1:trainLen+errorLen+1]))
-    print(nrmse(net.Y[0,0:errorLen],net.u[trainLen+1:trainLen+errorLen+1]))
     mi_i=memory_capacity_n(net.Y, net.u,n)
+    nrmse_i=nrmse_n(net.Y,net.u,i_max,errorLen,trainLen)
+    
+    #prints
+    print(nrmse(net.Y[0,0:errorLen],net.u[trainLen+1:trainLen+errorLen+1]))
     print(net.res_size, " FINISHED")
+    
     return X,Y,nrmse_i,mi_i
 
 
 # In[9]:
 
 
-def plot_MI_i(key,mi_dict,n):
+def plot_MI_i(key,mi_dict,nrmse=True, single=True):
     x=[]
     y=[]
+    
+    if nrmse:
+        point=min(mi_dict, key=mi_dict.get)
+        print("min",point)
+    else:
+        point=max(mi_dict, key=mi_dict.get)
+        print("max",point)
+        
     for i,MI in mi_dict.items():
         x.append(i)
         y.append(MI)
+    
     plot(x,y,label=key)
-    plot(n,mi_dict[n],marker='o')
+    plot(point,mi_dict[point],marker='o')
+    
+    if single:
+        xlabel("i=[0,%d]"%(x[-1]))
+        ylabel("NRMSE(X(t-i), Y(t))")
 
 
 # In[10]:
@@ -169,16 +177,31 @@ def plot_MI_by_file(MI_by_file):
 # In[11]:
 
 
+def plot_temporal_lines(u,Y,n,length,filename, save=True):
+    plot( u[trainLen+1-n:trainLen+length+1-n], 'g' )
+    plot( Y.T[0:length], 'b' )
+    xlabel("time")
+    ylabel("signal")
+    title('Target and generated signals $y(t)$ starting at $t=0$ until $t=%s$ with a delay of n=%d ' %(length,n))
+    legend(['Target signal', 'Free-running predicted signal'])
+    if save:
+        savefig("plots/input-vs-output/%s_len%d_n%d" %(filename,length,n))
+    show()
+
+
+# In[ ]:
+
+
 ##################################################################################
 
 
-# In[12]:
+# In[ ]:
 
 
 #                                  PARAMETERS                                    #
 
 
-# In[13]:
+# In[12]:
 
 
 # TRAINING AND TEST LENGHT
@@ -264,14 +287,14 @@ net.X[:,7]
 
 
 print("With derivatives")
-MI_by_file, X_by_file, Y_by_file, NRMSE_by_file=testing_gene_net_derivative("Dataset1/", a,b,c,20)
+X_by_file, Y_by_file, NRMSE_by_file, MI_by_file=testing_gene_net_derivative("Dataset1/", a,b,c,20)
 
 
 # In[ ]:
 
 
 for n in [0,10,20,50,80,100]:
-    MI_by_file, X_by_file, Y_by_file, NRMSE_by_file=testing_gene_net_derivative("Dataset1/", a,b,c,n)
+    X_by_file, Y_by_file, NRMSE_by_file,MI_by_file=testing_gene_net_derivative("Dataset1/", a,b,c,n)
     figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
     title("n="+str(n))
     plot_MI_by_file(MI_by_file)
@@ -289,24 +312,12 @@ for n in [0,10,20,50,80,100]:
 #una n
 
 
-# In[21]:
+# In[ ]:
 
 
 X,Y,nrmse_i,mi_i=testing_gene_net_derivative_file("Dataset1",csv_files[-1],a=a,b=b,c=c,n=20)
 plot_MI_i("DBTBS", nrmse_i,n=20)
 show()
-
-
-# In[19]:
-
-
-min(nrmse_i, key=nrmse_i.get)
-
-
-# In[17]:
-
-
-max(nrmse_i, key=nrmse_i.get)
 
 
 # In[ ]:
@@ -315,21 +326,26 @@ max(nrmse_i, key=nrmse_i.get)
 #rango de n
 
 
-# In[24]:
+# In[13]:
 
 
-for n in [0,15,20,25,30,50]:
-    X,Y,nrmse_i,mi_i=testing_gene_net_derivative_file("Dataset1",csv_files[-1],a=a,b=b,c=c,n=n)
-    print("min",min(nrmse_i, key=nrmse_i.get))
-    figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
+file=csv_files[-1]
+filename=file[file.index("list")+5:file.index(".csv")]
+
+for n in [0,15,20,25,30,50,60,80]:
+    X,Y,nrmse_i,mi_i=testing_gene_net_derivative_file("Dataset1",file,a=a,b=b,c=c,n=n)
     
+    figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
     title("n="+str(n))
-    plot_MI_i("DBTBS", nrmse_i,n)
+    plot_MI_i(filename, nrmse_i)
+    savefig("plots/nrmse_i/%s_n%d" %(filename,n))
     show()
-
-
-# In[ ]:
-
-
-show()
+    
+    figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
+    plot_temporal_lines(X,Y, n, testLen,filename)
+    
+    
+    figure(num=None, figsize=(10, 8), dpi=80, facecolor='w', edgecolor='k')
+    plot_temporal_lines(X,Y, n, 50,filename)
+   
 
